82f41bcf2974cdc1d0954f7c6b7aa617c5fa65b9
Serhii Fesenko <serhiifesenko.dev@gmail.com>
2021-03-14 22:20:23 +0200

    Update App component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
+import { CssBaseline } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import AppRouter from "./AppRouter";
 import { StoreProvider } from "./store";
 
-function App() {
+const useStyles = makeStyles({
+  root: {
+    height: "100vh",
+    width: "100vw",
+    display: "flex",
+  },
+});
+
+const App = () => {
+  const classes = useStyles();
+
   return (
     <StoreProvider>
-      <div className="App"></div>;
+      <CssBaseline />
+      <div className={classes.root}>
+        <AppRouter />
+      </div>
     </StoreProvider>
   );
-}
+};
 
 export default App;
